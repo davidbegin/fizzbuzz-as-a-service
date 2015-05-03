843ba82fdc90c7222ed4aa394852492511db0b36
David Begin <davidmichaelbe@gmail.com>
2015-05-03 00:53:35 -0700

    just some refactor on kaizen saturday

--- scripts/example2.rb
+++ scripts/example2.rb
@@ -3,16 +3,16 @@ require "uri"
 
 (1..100).each do |number|
   fizz_url = URI.parse("http://localhost:1987/fizz?num=#{number}")
-  divisible_by_3 = Net::HTTP.get_response(fizz_url).body
+  divisible_by_3 = Net::HTTP.get_response(fizz_url).body == "true"
 
   buzz_url = URI.parse("http://localhost:1987/buzz?num=#{number}")
-  divisible_by_5 = Net::HTTP.get_response(buzz_url).body
+  divisible_by_5 = Net::HTTP.get_response(buzz_url).body == "true"
 
-  output = if divisible_by_3 == "true" && divisible_by_5 == "true"
+  output = if divisible_by_3 && divisible_by_5
     "FizzBuzz"
-  elsif divisible_by_3 == "true"
+  elsif divisible_by_3
     "Fizz"
-  elsif divisible_by_5 == "true"
+  elsif divisible_by_5
     "Buzz"
   else
     number.to_s
