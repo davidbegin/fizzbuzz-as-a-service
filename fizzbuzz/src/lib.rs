f44e4c7f3ac79781c6924cd9eea38daeeb7dc1fc
David Begin <davidmichaelbe@gmail.com>
2015-05-04 21:20:16 -0700

    fuck it ship it

--- fizzbuzz/src/lib.rs
+++ fizzbuzz/src/lib.rs
@@ -1,3 +1,36 @@
+#![allow(dead_code)]
+
+pub fn fizzbuzz_converter(num: i32) -> String {
+    let divisible_by_3_eh: bool = num % 3 == 0;
+    let divisible_by_5_eh: bool = num % 5 == 0;
+
+    if divisible_by_3_eh && divisible_by_5_eh {
+        "FizzBuzz".to_string()
+    } else if divisible_by_3_eh {
+        "Fizz".to_string()
+    } else if divisible_by_5_eh {
+        "Buzz".to_string()
+    } else {
+        num.to_string()
+    }
+}
+
 #[test]
-fn it_works() {
+fn nine_is_fizz() {
+    assert_eq!(fizzbuzz_converter(9), "Fizz".to_string());
+}
+
+#[test]
+fn fifteen_is_fizzbuzz() {
+    assert_eq!(fizzbuzz_converter(15), "FizzBuzz".to_string());
+}
+
+#[test]
+fn ten_is_buzz() {
+    assert_eq!(fizzbuzz_converter(10), "Buzz".to_string());
+}
+
+#[test]
+fn eleven_is_eleven() {
+    assert_eq!(fizzbuzz_converter(11), "11".to_string());
 }
--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,15 +1,28 @@
 #[macro_use] extern crate nickel;
 
 use nickel::Nickel;
+use nickel::router::http_router::HttpRouter;
+extern crate fizzbuzz;
 
 fn main() {
     let mut server = Nickel::new();
+
+    server.get("/fizzbuzz/:num", middleware! { |request|
+        let num_param = request.param("num");
+        let num: i32 = num_param.parse().unwrap();
+        let result = fizzbuzz::fizzbuzz_converter(num);
+        format!("{}", result)
+    });
+
+    server.get("/fizz", middleware!("Fizz"));
+    server.get("/buzz", middleware!("Buzz"));
 
     server.utilize(router! {
         get "**" => |_req, _res| {
             "Rust FizzBuzz coming soon!"
         }
     });
+
 
     server.listen("127.0.0.1:1987");
 }
